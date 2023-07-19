02ee4682a4ce1b55d6bb7e8c5ef85c283c9e8501
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-19 09:51:46 -0300

    Update id.dart

--- lib/dart/id.dart
+++ lib/dart/id.dart
@@ -1,1 +1,15 @@
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final didUrl = 'did:example:123456789abcdefghi';
+  final resolverUrl = 'https://example-did-resolver.com/resolve';
+
+  final response = await http.get('$resolverUrl?did=$didUrl');
+  if (response.statusCode == 200) {
+    final didDocument = response.body;
+    print('Resolved DID Document: $didDocument');
+  } else {
+    print('Failed to resolve DID. Status code: ${response.statusCode}');
+  }
+}
 
