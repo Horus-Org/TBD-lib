56fa5b1927d2d60439d20bdd681081d2419fd877
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-19 09:52:28 -0300

    Update id.ts

--- lib/typescript/id.ts
+++ lib/typescript/id.ts
@@ -1,1 +1,17 @@
+import { Resolver } from 'did-resolver';
+import fetch from 'node-fetch';
+
+async function resolveDid(didUrl: string, resolverUrl: string) {
+  const resolver = new Resolver({ fetch });
+  try {
+    const didDocument = await resolver.resolve(didUrl);
+    console.log('Resolved DID Document:', didDocument);
+  } catch (error) {
+    console.error('Failed to resolve DID:', error);
+  }
+}
+
+const didUrl = 'did:example:123456789abcdefghi';
+const resolverUrl = 'https://example-did-resolver.com/resolve';
+resolveDid(didUrl, resolverUrl);
 
